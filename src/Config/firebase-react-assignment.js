ed8fe8d0fbb7c971fd8d2506e53704b45b71b95e
HappyArava <42187365+HappyArava@users.noreply.github.com>
2019-02-02 19:28:52 +0530

    Firebase Details Hidden

--- src/Config/firebase-react-assignment.js
+++ src/Config/firebase-react-assignment.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase';
 var config = {
-    apiKey: "AIzaSyARs0JhFunnXzajVrXlV_4ilnQoZuGieRQ",
-    authDomain: "react-assignment-3889e.firebaseapp.com",
-    databaseURL: "https://react-assignment-3889e.firebaseio.com",
-    projectId: "react-assignment-3889e",
-    storageBucket: "react-assignment-3889e.appspot.com",
-    messagingSenderId: "576380880451"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   };
 firebase.initializeApp(config);
 const database = firebase.database();
